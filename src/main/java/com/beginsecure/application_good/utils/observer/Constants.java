92c57f9ce27ec903d05c92d72947fcb9b09f7e4f
Sc0ppq <tibiloana@yahoo.com>
2025-03-31 12:52:38 +0300

    comm

---
+++ src/main/java/com/beginsecure/application_good/utils/observer/Constants.java
@@ -0,0 +1,7 @@
+package com.beginsecure.application_good.utils.observer;
+
+import java.time.format.DateTimeFormatter;
+
+public class Constants {
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+}
